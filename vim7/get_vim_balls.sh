abab77e63ffca7f33a0393ccad12624f5d777592
Taylor Carpenter <taylor@codecafe.com>
2013-09-17 03:32:03 -0500

    more cleanup for old vim7 only files

--- vim7/get_vim_balls.sh
+++
@@ -1,6 +0,0 @@
-DL=curl
-DLOPTS="-O"
-
-$DL $DLOPTS http://mysite.verizon.net/astronaut/vim/vbafiles/netrw.vba.gz
-$DL $DLOPTS http://mysite.verizon.net/astronaut/vim/vbafiles/ZoomWin.vba.gz
-$DL $DLOPTS http://mysite.verizon.net/astronaut/vim/vbafiles/twl.vba.gz
